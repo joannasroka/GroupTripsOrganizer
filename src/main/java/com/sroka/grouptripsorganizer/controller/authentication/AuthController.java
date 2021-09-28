1403514129fc144f2d91e38cf2282b6a07ef577b
joannasroka <joannasroka98@gmail.com>
2021-09-28 21:09:44 +0200

    Move to package 'controller'

--- src/main/java/com/sroka/grouptripsorganizer/controller/authentication/AuthController.java
+++ src/main/java/com/sroka/grouptripsorganizer/controller/authentication/AuthController.java
@@ -1,4 +1,4 @@
-package com.sroka.grouptripsorganizer.authentication;
+package com.sroka.grouptripsorganizer.controller.authentication;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
