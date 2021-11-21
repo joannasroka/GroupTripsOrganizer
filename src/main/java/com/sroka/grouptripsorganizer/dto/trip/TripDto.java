a32ee6e33422684628b921baa908476444786e5c
joannasroka <joannasroka98@gmail.com>
2021-11-21 13:11:25 +0100

    Add set of participants to TripDto

--- src/main/java/com/sroka/grouptripsorganizer/dto/trip/TripDto.java
+++ src/main/java/com/sroka/grouptripsorganizer/dto/trip/TripDto.java
@@ -1,9 +1,12 @@
 package com.sroka.grouptripsorganizer.dto.trip;
 
+import com.sroka.grouptripsorganizer.dto.user.UserDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -14,4 +17,7 @@ public class TripDto {
 
     @Schema(required = true)
     private String name;
+
+    @Schema(required = true)
+    Set<UserDto> participants;
 }
